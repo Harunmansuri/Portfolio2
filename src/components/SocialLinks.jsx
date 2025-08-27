8d10e2a91c01cf494be3c8ea905c736340c72446
Harun mansuri <mrharun961766@gmail.com>
2025-08-27 16:06:40 +0530

    some setup

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -10,7 +10,7 @@ const SocialLinks = () => {
             id: 1,
             child: (
                 <>
-                LinkedIn <FaLinkedin size={30} />
+                    LinkedIn <FaLinkedin size={30} />
                 </>  
             ),
             href: 'https://www.linkedin.com/in/mausoomi-roy/',
@@ -20,7 +20,7 @@ const SocialLinks = () => {
             id: 2,
             child: (
                 <>
-                GitHub <FaGithub size={30} />
+                    GitHub <FaGithub size={30} />
                 </>  
             ),
             href: 'https://github.com/Mausoomi12',
@@ -29,50 +29,49 @@ const SocialLinks = () => {
             id: 3,
             child: (
                 <>
-                Mail <HiOutlineMail size={30} />
+                    Mail <HiOutlineMail size={30} />
                 </>  
             ),
             href: 'mailto:mausoomiroy68613@gmail.com',
-            style: 'rounded-tr-md'
         },
         {
             id: 4,
             child: (
                 <>
-                Resume <BsFillPersonLinesFill size={30} />
+                    Resume <BsFillPersonLinesFill size={30} />
                 </>  
             ),
             href: '/Mausoomi_Roy.pdf',
-            style: 'rounded-tr-md',
+            style: 'rounded-br-md',
             download: true,
         },
     ]
 
-  return (
-    <div className="hidden lg:flex flex-col top-[35%] left-0 fixed">
-        <ul>
-
-            {links.map(({id, child, href, style, download}) => (
-                 <li
-                  key={id} 
-                  className={
-                    "flex justify-between items-center w-40 h-14 px-4 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-300 bg-gray-500" +
-              " " + 
-                  style}>
-                 <a 
-                 href={href} 
-                 className="flex justify-between items-center w-full text-white"
-                 download={download}
-                 target='_blank'
-                 rel="noreferrer">
-                    {child}
-                 </a>
-             </li>
-            ))}
-           
-        </ul>
-    </div>
-  )
+    return (
+        <div className="hidden lg:flex flex-col top-[35%] left-0 fixed z-50">
+            <ul>
+                {links.map(({id, child, href, style, download}) => (
+                    <li
+                        key={id} 
+                        className={
+                            "flex justify-between items-center w-44 h-14 px-4 ml-[-110px] hover:ml-[-10px] duration-500 ease-in-out bg-black text-white hover:bg-gray-800 shadow-lg hover:shadow-2xl transform hover:scale-105" +
+                            " " + style
+                        }
+                    >
+                        <a 
+                            href={href} 
+                            className="flex justify-between items-center w-full"
+                            download={download}
+                            target='_blank'
+                            rel="noreferrer"
+                        >
+                            {child}
+                        </a>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    )
 }
 
 export default SocialLinks;
